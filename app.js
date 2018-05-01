7f77fc29741e832fd51b818e2a6f8cea8f3232dd
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-05-01 15:01:19 +0000

    add results page view

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+app.set("view engine", "ejs");
+
 app.get("/results", function(req, res){
-    request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+    request("http://www.omdbapi.com/?s=iowa&apikey=thewdb", function(error, response, body) {
         if(!error && response.statusCode == 200) {
-            var results = JSON.parse(body);
-            res.send(results["Search"][0]["Title"]);
+            var data = JSON.parse(body);
+            res.render("results", {data: data});
         }    
     });
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server working!")
+    console.log("Movie App has started!")
 })
 
 
